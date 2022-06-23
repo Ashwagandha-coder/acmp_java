dd146268e6450cd1d3e30d97e2f9ee01ce955ada
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-23 19:17:58 +0500

    ready task and test Binary

---
+++ src/tasks/Бинарные_числа/Binary.java
@@ -0,0 +1,15 @@
+package tasks.Бинарные_числа;
+
+import java.util.Scanner;
+
+public class Binary {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        new Do().doSome(scanner.nextInt());
+
+    }
+}
+
