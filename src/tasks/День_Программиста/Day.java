4fbe4b8acc8ab7d19daac6071195caa8a45aeefc
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-26 12:21:25 +0500

    ready tak and test ProgrammerDay

---
+++ src/tasks/День_Программиста/Day.java
@@ -0,0 +1,15 @@
+package tasks.День_Программиста;
+
+public class Day {
+
+
+    public static void main(String[] args) {
+
+        ProgrammerDay programmerDay = new ProgrammerDay();
+        programmerDay.scannerValue();
+        System.out.println(programmerDay.compareYear());
+
+
+    }
+}
+
