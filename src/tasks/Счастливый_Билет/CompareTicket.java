e2c388511ec45fc4c17f6b6d4edc8b28cbf7a363
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-26 12:45:57 +0500

    refactoring

--- src/tasks/Счастливый_Билет/CompareTicket.java
+++ src/tasks/Счастливый_Билет/CompareTicket.java
@@ -1,12 +1,11 @@
 package tasks.Счастливый_Билет;
 
-import java.util.ArrayList;
 
 public class CompareTicket {
 
     public String compareHappyTicket(int ticket) {
 
-        //ArrayList<Integer> arrayList = new ArrayList<>();
+
         int count = 0;
 
         int[] masive = new int[6];
@@ -18,7 +17,7 @@ public class CompareTicket {
             count++;
 
         }
-       // arrayList.trimToSize();
+
 
         if (masive[0] + masive[1] + masive[2] == masive[3] + masive[4] + masive[5])
             return "YES";
